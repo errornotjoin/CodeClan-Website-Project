d5958c65b4b944d8155eb7af0739f16a19ae483a
ErrorNotJoin <joshuahastings29@gmail.com>
2023-08-04 11:36:37 +0100

    Saving

---
+++ index_Js/Hidding_the_items.js
@@ -0,0 +1,9 @@
+
+function Truning_all_off()
+{
+    for(var x = 0; x <= 4; x++)
+    {
+        var Meun_items = document.getElementById("Main_Items_" + 4)
+        Meun_items.style.display = "none"
+    }
+}
